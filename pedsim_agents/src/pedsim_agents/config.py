c539cf4b18348b30c9d94b3415be9d71e2224485
Vova Shcherbyna <volodymyr.shcherbyna@alumni.tu-berlin.de>
2024-02-23 11:21:17 +0100

    divest

--- pedsim_agents/src/pedsim_agents/config.py
+++
@@ -1,4 +0,0 @@
-class Topics:
-    INPUT: str = "input"
-    FEEDBACK: str = "feedback"
-    SEMANTIC: str = "semantic"
--- pedsim_agents/src/pedsim_agents/forces/forcemodels/__init__.py
+++
@@ -1,5 +0,0 @@
-from .deepsocialforce import main # noqa
-from .evacuation import main # noqa
-from .passthrough import main # noqa
-from .pysocial import main # noqa
-from .spinny import main # noqa
